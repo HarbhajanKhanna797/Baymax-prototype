51be9797ed7643e20e46a0742c4832ef36e5fab2
HarbhajanKhanna797 <harbhajankhanna797@gmail.com>
2025-06-16 14:54:48 +0530

    added text to speech and a manual timer

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from api import get_health_suggestions
-from stt import speech_to_text
+from stt import speech_to_text, speak
 from face_detection import face_detection
+from manualTimer import ManualTimer
 
 import time
 import speech_recognition as sr # type: ignore
+from chatterbox.tts import ChatterboxTTS # type: ignore
+import contextlib
+import os
+
+with open(os.devnull, 'w') as f, contextlib.redirect_stdout(f):
+    import pygame #type: ignore
+
+import warnings
+from cryptography.utils import CryptographyDeprecationWarning # type: ignore
+
+warnings.filterwarnings("ignore", category=CryptographyDeprecationWarning)
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+model = ChatterboxTTS.from_pretrained(device="cuda")
+timer = ManualTimer()
+
+pygame.mixer.init()
 
 robot_commands = {
     "come here": "MOVE_FORWARD",
@@ -25,17 +45,36 @@ def route_command(command_text):
             return
 
     fever_phrases = ["fever", "hot", "shivering", "headache", "sweating", "chills"]
+    pain_phrases = ["pain", "hurt", "ache", "sore", "ow", "ouch"]
+
+    response = get_health_suggestions([command_text])
+    if any(phrase in command_text for phrase in pain_phrases):
+        sound = pygame.mixer.Sound("audio-files/pain_converted.wav")
+        channel = sound.play()
+        while channel.get_busy():
+            time.sleep(0.1)
+        time.sleep(2)  
 
     if any(phrase in command_text for phrase in fever_phrases):
         print("Let's check your temperature.")
+        sound = pygame.mixer.Sound("audio-files/temperature_converted.wav")
+        channel = sound.play()
+        while channel.get_busy():
+            time.sleep(0.1)
         if face_detection():
             pass # Face detected, proceed with temperature check
         else:
             print("No face detected. Unable to check temperature.")
 
     # If no robot command matched, treat it as a health-related input
-    response = get_health_suggestions([command_text])
     print("Baymax response:", response)
+    speak(model, response)
+    time.sleep(2)  # Wait for the response to finish
+
+    sound = pygame.mixer.Sound("audio-files/sleep_converted.wav")
+    channel = sound.play()
+    while channel.get_busy():
+        time.sleep(0.1)
 
 def trigger_robot_action(action):
     # You can later send serial data to Arduino or control OpenCV gesture
@@ -44,34 +83,41 @@ def trigger_robot_action(action):
 recognizer = sr.Recognizer()
 mic = sr.Microphone()
 
-WAKE_WORDS = ["bayma", "baymax", "payamax", "i max", "d max"]
-AWAKE_DURATION = 25
+WAKE_WORDS = ["bayma", "baymax", "paymax", "payma", "imax", "dmax", "i max", "d max", "bama", "remix", "vmax"]
+AWAKE_DURATION = 60
 
 while True:
     print("Waiting for wake word...")
     try:
+        timer.reset()
         wake_command = speech_to_text(recognizer, mic)
+        print(f"Wake command received: {wake_command}")
 
-        if (WAKE_WORD.lower() for WAKE_WORD in WAKE_WORDS) in wake_command.lower():
+        if any(wake_word in wake_command for wake_word in WAKE_WORDS):
             print("Wake word detected. Activating Baymax...")
+            sound = pygame.mixer.Sound("audio-files/intro_converted.wav")
+            channel = sound.play()
+            while channel.get_busy():
+                time.sleep(0.1)
+            print("Enter awake loop")
+            timer.start()
             awake_until = time.time() + AWAKE_DURATION
 
-            while time.time() < awake_until:
+            while timer.get_elapsed_time() < AWAKE_DURATION:
                 print("Listening for commands...")
                 command_text = speech_to_text(recognizer, mic)
 
-                if command_text != "i am satisfied with my care":
+                if "satisfied" not in command_text.lower():
                     print(f"Command received: {command_text}")
-                    awake_until = time.time() + AWAKE_DURATION
                     route_command(command_text)
+                    timer.reset()
+                    timer.start()
 
                 else:
                     print("Patient is satisfied. Baymax will go to sleep.")
+                    timer.reset()
                     break
 
-            if time.time() >= awake_until:
-                print("Baymax is going to sleep now.")
-                break
     except Exception as e:
         print(f"Error during wake word detection: {e}")
         continue
--- stt.py
+++ stt.py
@@ -1,6 +1,9 @@
 import speech_recognition as sr # type: ignore
 import time
-from api import get_health_suggestions  # seconds Baymax stays awake after activation
+import torchaudio as ta # type: ignore
+from pydub import AudioSegment # type: ignore
+from playsound import playsound # type: ignore
+import os
 
 def speech_to_text(recognizer, mic):
     with mic as source:
@@ -17,6 +20,30 @@ def speech_to_text(recognizer, mic):
             print("Could not reach the speech recognition service.")
         except sr.WaitTimeoutError:
             print("No speech detected. Still awake...")
+
+def generate_speech_to_text(model, text):
+    AUDIO_PROMPT_PATH = "audio-files/baymax_clip_1_clean.wav"
+    wav = model.generate(text, audio_prompt_path=AUDIO_PROMPT_PATH)
+    ta.save("temp.wav", wav, model.sr)
+
+def convert_to_pcm(filename):
+    audio = AudioSegment.from_file(filename + ".wav", format="wav")
+    audio = audio.set_sample_width(2)  # 2 bytes = 16-bit
+    audio = audio.set_channels(1)      # mono
+    audio = audio.set_frame_rate(16000)
+    audio.export(filename + "_converted.wav", format="wav")
+
+def speak(model, text):
+    print("Baymax says:", text)
+    generate_speech_to_text(model, text)
+
+    # Play the generated audio
+    convert_to_pcm("temp")
+    if os.path.exists("temp.wav"):
+        os.remove("temp.wav")
+    playsound("temp_converted.wav")
+    if os.path.exists("temp_converted.wav"):
+        os.remove("temp_converted.wav")
 
 if __name__ == "__main__":
     recognizer = sr.Recognizer()
